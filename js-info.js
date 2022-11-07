645df345fb9b2788e9b4bbed13e3d1f16c93fb80
nickosha <68966962+nickosha@users.noreply.github.com>
2022-11-07 10:16:16 +0300

    init: start first-project

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Брендан Эйх')
+alert('JavaScript был создан за 10 дней')
+alert('Javascript вскоре появился в Netscape Navigator 2.0 в сентябре 1995 года.')
+alert('JavaScript - это не Java')
+alert('Спецификация языка JavaScript называется ECMAScript.')
